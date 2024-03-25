ec064e8a17d14a83a5ee90323e3212a05d7e792f
USER <nguyentuanvu1410@gmail.com>
2024-03-25 20:14:41 +0700

    commit repo

--- src/main/java/com/mycompany/spring_mvc_project_final/repository/ShowTimeRepository.java
+++ src/main/java/com/mycompany/spring_mvc_project_final/repository/ShowTimeRepository.java
@@ -1,14 +1,9 @@
 package com.mycompany.spring_mvc_project_final.repository;
 
-import java.time.LocalDate;
-import java.util.Date;
-import java.util.List;
-
 import com.mycompany.spring_mvc_project_final.entities.ShowTime;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.repository.CrudRepository;
 
-@Repository
-public interface ShowTimeRepository extends JpaRepository<ShowTime, Long> {
-    List<ShowTime> findByShowDate(Date showDate);
+public interface ShowtimeRepository extends CrudRepository<ShowTime, Long> {
+    ShowTime findByShowTimeId(Long showTimeId);
+    
 }
